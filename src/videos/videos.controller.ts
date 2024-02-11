b5c5ec9bd10fbf0ed5962105a54b6c719c5fccb0
mfranmendoza <mfranmendoza@falabella.cl>
2024-02-11 01:03:49 -0300

    feat(videos.controller): add method for get services

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { VideosService } from './videos.service';
+import { Response } from 'express';
 
 @Controller('videos')
-export class VideosController {}
+export class VideosController {
+  constructor(private readonly videosService: VideosService) {}
+
+  @Get()
+  getAllVideos() {
+    return this.videosService.getallVideos();
+  }
+
+  @Get('last')
+  getLastVideo(@Res() res: Response){
+    const video = this.videosService.getLastVideo();
+    const stream = this.videosService.getVideoByName(video.name);
+    stream.pipe(res);
+  }
+
+  @Get(':name')
+  getVideoByName(@Param('name') name: string, @Res() res: Response){
+    const stream = this.videosService.getVideoByName(name);
+    stream.pipe(res);
+  }
+}
